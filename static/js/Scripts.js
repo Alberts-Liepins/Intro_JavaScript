e1a21655a317f37edfe77bec8d6968affff45a75
Alberts-Liepins <liepins01@example.com>
2021-03-02 14:19:20 +0200

    JS

---
+++ static/js/Scripts.js
@@ -0,0 +1,9 @@
+console.log("Viss strādā")
+//alert("Sveiki!")
+alert("JavaScript")
+
+let variable_text = "skaits"
+let count = 20
+
+console.log(variable_text)
+alert("Pašlaik "+variable_text+" ir "+count)
